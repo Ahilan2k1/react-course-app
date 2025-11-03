6ac525a925e59042abc5b31772b989be8a095c93
Poojashree Elangovan <poojashreeelangovan@Poojashrees-MacBook-Air.local>
2025-11-03 09:02:55 +0530

    Day 1

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement('h1', { id: 'heading', xyz: 'abc' }, 'Hello world');
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(root);
+root.render(heading);
+console.log(root.render(heading));
