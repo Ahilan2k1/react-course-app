82437225d57371a8aad22a16ad3e2dcce9b6ef9e
Poojashree Elangovan <poojashreeelangovan@Poojashrees-MacBook-Air.local>
2025-12-09 22:05:21 +0530

    Zyraa Demo UI

--- App.js
+++
@@ -1,6 +0,0 @@
-const heading = React.createElement('h1', { id: 'heading', xyz: 'abc' }, 'Hello world');
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(root);
-root.render(heading);
-console.log(root.render(heading));
